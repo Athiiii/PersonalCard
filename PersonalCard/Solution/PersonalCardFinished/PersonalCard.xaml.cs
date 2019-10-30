34ffbab67ef45754b85d80ab9716e64fd516bceb
Athiiii <theivakulasingham@outlook.de>
2019-10-30 07:50:52 +0100

    .

---
+++ PersonalCard/Solution/PersonalCardFinished/PersonalCard.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+using Image = System.Windows.Controls.Image;
+
+namespace PersonalCardFinished
+{
+    /// <summary>
+    /// Interaction logic for PersonalCard.xaml
+    /// </summary>
+    public partial class PersonalCard : UserControl
+    {
+        public PersonalCard()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public string FullName { get; set; }
+        public string Tel { get; set; }
+        public string Mail { get; set; }
+        public string Adress { get; set; }
+        public Image ImageUrl { get; set; }
+
+    }
+}
